57b1bd1f481f6b358ca875ff906f2ced5544d7d0
AzaWoodyy <aza.woodyy@gmail.com>
2023-04-04 08:53:48 +0200

    Initial Commit

---
+++ AppTpSwift/AppTpSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTpSwiftApp.swift
+//  AppTpSwift
+//
+//  Created by Enzo Neault on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppTpSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
